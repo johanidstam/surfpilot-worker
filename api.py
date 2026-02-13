530007f4699c77fe2e777d78157b5e2bb686d569
johanidstam <johan.idstam@gmail.com>
2026-02-13 14:19:46 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,16 +1,19 @@
 from flask import Flask, jsonify
-import json
 import os
-
+import json
 
 app = Flask(__name__)
 
-DATA_FILE = "/data/data.json"
+DATA_FILE = "data.json"
+
+
+@app.route("/")
+def home():
+    return "API is running"
 
 
 @app.route("/data")
 def data():
-
     if not os.path.exists(DATA_FILE):
         return jsonify({"plans": []})
 
@@ -19,7 +22,5 @@ def data():
 
 
 if __name__ == "__main__":
-
     port = int(os.environ.get("PORT", 8080))
-
     app.run(host="0.0.0.0", port=port)
